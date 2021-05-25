fd4791a2d5c5b37cb27d9aada4186376a114b04a
sai1004 <saikiran.bavandla@gmail.com>
2021-05-25 19:53:48 +0530

    commit

--- nodejs-auth-app/src/services/ProfileService.ts
+++ nodejs-auth-app/src/services/ProfileService.ts
@@ -14,7 +14,7 @@ export class ProfileService {
 
     async saveProfile(item: Profile) {
         try {
-            let isValid = await this.validateUser(item);
+            let isValid = await this.validateProfile(item);
             if (isValid == true) {
                 item.password = this.HashSync(item.password);
                 item.token = "dksjhdkfhsdfkjhsldjkfhskdjfhsd";
@@ -39,7 +39,9 @@ export class ProfileService {
         return hashSync(data, 8);
     }
 
-    async validateUser(item: Profile) {
+    async validateProfile(item: Profile) {
+        let returnVal: any;
+
         let oldProfile = null;
         // check profile id
         if (!item.id || item.id == "0" || item.id == "") {
@@ -48,22 +50,26 @@ export class ProfileService {
             oldProfile = await this.profileDao.findOne(item.id);
         }
 
-        // get email
-        let email: any = await this.profileDao.search({ email: item.email });
+        // get email if exists in array
+        let profiles: any = await this.profileDao.search({ email: item.email });
 
-        // check email
+        // if id not exists
         if (!item.id) {
-            if (item.email != email) {
+            // check profile
+            if (profiles.length > 0) {
+                returnVal = "Email";
+            } else {
+                // genrate new id and profile if there is no old record associated with current email
                 let uid = App.UniqueCode();
                 item.id = uid;
-            } else {
-                return "Email";
+                returnVal = true;
             }
         }
 
         item.updatedBy = "system";
         let date = new Date().toISOString();
         item.updatedOn = new Date(date);
-        return true;
+
+        return returnVal;
     }
 }
